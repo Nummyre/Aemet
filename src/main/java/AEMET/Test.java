1615da987a3a6cdb8059771b765eed0f6d53065e
cintia <cintiia.349@gmail.com>
2019-05-17 18:56:17 +0200

    ProyectoAemet

---
+++ src/main/java/AEMET/Test.java
@@ -0,0 +1,40 @@
+package AEMET;
+
+import AEMET.model.HibernateUtil;
+import AEMET.model.entities.EstMeteorologia;
+import AEMET.model.entities.Estacion;
+import AEMET.model.entities.Provincia;
+import AEMET.model.logica.Controlador;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Cintia Ortiz
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+
+        ArrayList<Estacion> estaciones = Controlador.AllEstattions();
+        for (Estacion estacione : estaciones) {
+            try {
+                Controlador.insertEst_Met(estacione.getId());
+            } catch (Exception ex) {
+                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            }finally{
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+        System.out.println("Terminado!!!!!");
+        
+    }
+}
